51ab9de7ab14eef0961fc77b79baaaca705c51fb
external.aleksey.kudryavtse <external.aleksey.kudryavtse@distribusion.com>
2023-07-18 11:09:58 +0300

    Updates from teacher

--- lesson16_17_threads_processes_and_async/homework/async_server.py
+++ lesson16_17_threads_processes_and_async/homework/async_server.py
@@ -1,22 +1,12 @@
 import asyncio
 import json
-import threading
-from dataclasses import dataclass
 import multiprocessing
 
 import logging
-from typing import Callable
 
 from aiohttp import web, web_request
 
 logger = logging.getLogger(__name__)
-
-
-@dataclass(frozen=True)
-class AppHandler:
-    method: str
-    path: str
-    handler: Callable
 
 
 class ServerEmulator:
@@ -41,8 +31,8 @@ class ServerEmulator:
         logger.info(f"Starting the <{self.name}> at port {self._port}")
         web.run_app(self._app, port=self._port)
 
-    async def hello(self, _: web_request.Request) -> web.Response:
-        thread_name = threading.current_thread().name
+    async def hello(self, request: web_request.Request) -> web.Response:
+        requestor_name = request.query.get('requestor')
 
         self._calls_count += 1
         await self._QUEUE.put(self._calls_count)
@@ -55,16 +45,12 @@ class ServerEmulator:
         await asyncio.sleep(1)
 
         value = await self._QUEUE.get()
-        if thread_name != 'MainThread':
-            key = thread_name
-        else:
-            key = f'Async_{value}'
 
         return web.Response(
             status=200,
             text=json.dumps(
                 {
-                    "key": key,
+                    "key": requestor_name,
                     "value": value
                 }
             ),
@@ -72,7 +58,7 @@ class ServerEmulator:
         )
 
 
-def run_server(port: int = 8080):
+def run_server(port: int = 8000):
     def _inner():
         server = ServerEmulator(port=port)
         server.start()
--- lesson16_17_threads_processes_and_async/homework/homework.py
+++ lesson16_17_threads_processes_and_async/homework/homework.py
@@ -1,5 +1,7 @@
 import concurrent.futures as cf
 import asyncio
+import logging
+import threading
 import time
 
 import aiohttp
@@ -8,66 +10,78 @@ import requests
 from lesson16_17_threads_processes_and_async.helpers.custom_logger import setup_logging
 from lesson16_17_threads_processes_and_async.homework.async_server import run_server
 
-
 logger = setup_logging(__name__)
+logging.getLogger('urllib3.connectionpool').setLevel('ERROR')
+logging.getLogger('aiohttp.access').setLevel('ERROR')
 
 
 URL = "http://0.0.0.0"
 PORT = 8080
 HELLO_ENDPOINT = f"{URL}:{PORT}/hello"
-MAX_WORKERS = 3
+MAX_WORKERS = 20
 
 
 def send_request():
-    response = requests.get(url=HELLO_ENDPOINT)
+    response = requests.get(
+        url=HELLO_ENDPOINT,
+        params={'requestor': threading.current_thread().name}
+    )  # params is needed after Server implementation updates
     response.raise_for_status()
     data = response.json()
 
     return data['key'], data['value']
 
 
-def using_thread():
+def using_thread(requests_count: int):
     t = time.perf_counter()
     with cf.ThreadPoolExecutor(
-        max_workers=MAX_WORKERS
+        max_workers=requests_count,
+        thread_name_prefix='MyThread'
     ) as ex:
-        futs = [ex.submit(send_request) for _ in range(3)]
+        futs = [ex.submit(send_request) for _ in range(requests_count)]
 
         done_und_undone_futs = cf.wait(
-            futs, timeout=2
+            futs, timeout=10
         )
+        exec_time_1 = time.perf_counter() - t
+        print(f'exec_time_1: {exec_time_1}')
+
         passes, fails = [], []
-        for f in futs:
+        for f in done_und_undone_futs.done:
             try:
                 passes.append(f.result())
-            except RuntimeError as exc:
-                fails.append(type(exc), str(exc))
-        exec_time_1 = time.perf_counter()-t
-        print(f'exec_time_1: {exec_time_1}')
-        results_dict = [f.result() for f in done_und_undone_futs.done]
-        errors = [f.result() for f in done_und_undone_futs.not_done]
+            except requests.exceptions.RequestException as exc:
+                fails.append(exc)
+
         exec_time_2 = time.perf_counter() - t
         print(f'exec_time_2: {exec_time_2}')
-        print(f'results_dict: {results_dict}')
-        print(f'errors: {errors}')
 
-        assert (exec_time_1 and exec_time_2) < 10
+        # NO NEED TO PARSE the Undone futures!
 
-server = run_server(port=PORT)
-using_thread()
-time.sleep(5)
-server.kill()
+        # results_dict = [f.result() for f in done_und_undone_futs.done]
+        # errors = [f.result() for f in done_und_undone_futs.not_done]
+        # exec_time_2 = time.perf_counter() - t
+        # print(f'exec_time_2: {exec_time_2}')
 
-# async def send_request_async():
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get(url=HELLO_ENDPOINT) as resp:
-#             resp.raise_for_status()
-#             await asyncio.sleep(0.7)
-#             data = await resp.json()
-#
-#             return data['key'], data['value']
-#
-#
+        print(f'passes: {passes}')
+        print(f'errors: {fails}')
+
+        assert 0 < (exec_time_2 - exec_time_1) < 2
+
+
+async def send_request_async(request_number: int):  # argument is needed after Server implementation updates
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            url=HELLO_ENDPOINT,
+            params={'requestor': f'Async_{request_number}'}  # params is needed after Server implementation updates
+        ) as resp:
+            resp.raise_for_status()
+            await asyncio.sleep(0.7)
+            data = await resp.json()
+
+            return data['key'], data['value']
+
+
 # async def using_async():
 #     start_time = time.perf_counter()
 #     result = await asyncio.gather(
@@ -81,3 +95,11 @@ server.kill()
 # asyncio.run(using_async())
 # time.sleep(5)
 # server.kill()
+
+
+server = run_server(port=PORT)
+
+using_thread(requests_count=50)
+
+time.sleep(3)
+server.kill()
