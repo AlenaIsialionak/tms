c12d634f4f378a4f3442fe7b8e9436ddd0d39cd3
external.aleksey.kudryavtse <external.aleksey.kudryavtse@distribusion.com>
2023-08-03 10:08:21 +0300

    LinkedList implementation

---
+++ data_structures/LinkedList.py
@@ -0,0 +1,165 @@
+class Node:
+    __slots__ = ("item", "next_item", "prev_item")
+
+    def __init__(self, item):
+        self.item = item
+        self.next_item = None
+        self.prev_item = None
+
+    def get_item(self):
+        return self.item
+
+    def set_item(self, new_item):
+        self.item = new_item
+
+    def get_next(self):
+        return self.next_item
+
+    def set_next(self, new_next):
+        self.next_item = new_next
+
+
+class LinkedList:
+    __slots__ = ("head", "tail", "_name", "_size")
+
+    def __init__(self):
+        self.head = self.tail = None
+        self._name = self.__class__.__name__
+        self._size = 0
+
+    def __iter__(self):
+        """
+        Please implement it by yourself :)
+        """
+
+    def __str__(self):
+        """
+        Please implement it by yourself after __iter__:)
+        """
+        # return f"{self._name}({tuple(i for i in self)})"
+
+    def __contains__(self, item):
+        for element in self:
+            if element == item:
+                return True
+        return False
+
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def index(self, item):
+        current, counter = self.head, 0
+
+        while counter < self._size:
+            if current.get_item() == item:
+                return counter
+            current = current.get_next()
+            counter += 1
+
+        if counter == self._size - 1:
+            raise ValueError
+
+    def add(self, item):
+        """
+        Item is added to the Head of the List
+
+        Please implement it by yourself :)
+        NOTE: adding to the HEAD of LinkedList
+        should have complexity O(1)
+        """
+
+    def append(self, item):
+        """
+        Item is added to the Tail of the List
+        """
+        node = Node(item)
+
+        if self._size == 0:
+            node.set_next(self.head)
+            self.head = self.tail = node
+        else:
+            self.tail.set_next(node)
+            self.tail = node
+
+        self._size += 1
+
+    def insert(self, item, position):
+        if self._size == 0 or position <= 0:
+            self.add(item)
+        elif position > self._size - 1:
+            self.append(item)
+        else:
+            node = Node(item)
+            current, counter = self.head, 0
+            previous = None
+
+            while counter < self._size:
+                if counter == position:
+                    break
+                previous = current
+                current = current.get_next()
+                counter += 1
+
+            if previous is None:
+                node.set_next(self.head)
+                self.head = node
+            else:
+                node.set_next(current)
+                previous.set_next(node)
+
+            self._size += 1
+
+    def __update_prev_and_next(self, prev, curr):
+        if prev is None:
+            self.head = curr.get_next()
+        else:
+            next_ = curr.get_next()
+            if next_ is None:
+                self.tail = prev
+            prev.set_next(next_)
+
+    def pop(self, position=None):
+        if self._size == 0:
+            raise IndexError(f"pop from empty {self._name}")
+
+        if position is not None and (position < 0 or position >= self._size):
+            raise IndexError(f"pop index {position} is out of range")
+        elif position is None:
+            position = self._size - 1
+
+        current, counter = self.head, 0
+        previous = None
+
+        while counter < self._size:
+            if counter == position:
+                break
+            previous = current
+            current = current.get_next()
+            counter += 1
+
+        self.__update_prev_and_next(previous, current)
+        self._size -= 1
+
+        return current.get_item()
+
+    def remove(self, item):
+        current = self.head
+        previous, found = None, False
+        counter = 0
+
+        while counter < self._size:
+            if current.get_item() == item:
+                found = True
+                break
+            previous = current
+            current = current.get_next()
+            counter += 1
+
+        if not found:
+            raise ValueError(f"remove error: {item} not in {self._name}")
+        else:
+            self.__update_prev_and_next(previous, current)
+            self._size -= 1
---
+++ data_structures/check_linked_list.py
@@ -0,0 +1,364 @@
+from LinkedList import LinkedList
+
+
+def check_linked_list_methods(my_list):
+
+    # ADD
+    my_list.add("ADD_1")
+    print(my_list, "Size: {}".format(len(my_list)))
+
+    my_list.add("ADD_2")
+
+    # APPEND
+    my_list.append(1)
+    print(
+        my_list,
+        "Size: {}".format(len(my_list)),
+        "Tail: {}".format(my_list.tail.get_item()),
+    )
+    my_list.append(2)
+    print(
+        my_list,
+        "Size: {}".format(len(my_list)),
+        "Tail: {}".format(my_list.tail.get_item()),
+    )
+    my_list.append(3)
+    print(
+        my_list,
+        "Size: {}".format(len(my_list)),
+        "Tail: {}".format(my_list.tail.get_item()),
+    )
+    my_list.append(4)
+    print(
+        my_list,
+        "Size: {}".format(len(my_list)),
+        "Tail: {}".format(my_list.tail.get_item()),
+    )
+    my_list.append(5)
+    print(
+        my_list,
+        "Size: {}".format(len(my_list)),
+        "Tail: {}".format(my_list.tail.get_item()),
+    )
+    print(my_list)
+
+    assert [i for i in my_list] == ["ADD_2", "ADD_1", 1, 2, 3, 4, 5]
+
+    # ADD
+    """
+    Uncomment the lines bellow after the add() method will be implemented 
+    """
+    # my_list.add("ADD_3")
+    # print(my_list, "Size: {}".format(len(my_list)))
+    # my_list.add("ADD_4")
+    # print(my_list, "Size: {}".format(len(my_list)))
+    # my_list.add("ADD_5")
+    # print(my_list, "Size: {}".format(len(my_list)))
+
+    # ASSERTIONS
+    assert len(my_list) == 10
+    assert [i for i in my_list] == [
+        "ADD_5",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        "ADD_1",
+        1,
+        2,
+        3,
+        4,
+        5,
+    ]
+    assert my_list.index("ADD_5") == 0
+    assert my_list.index(5) == len(my_list) - 1
+
+    # INSERT
+
+    # Insert to TAIL
+    my_list.insert("INSERT_1", 11)
+    # print(my_list, "Size: {}".format(len(my_list)))
+    assert [i for i in my_list] == [
+        "ADD_5",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        "ADD_1",
+        1,
+        2,
+        3,
+        4,
+        5,
+        "INSERT_1",
+    ]
+    assert my_list.tail.item == "INSERT_1"
+    assert len(my_list) == 11
+    assert my_list.index("INSERT_1") == len(my_list) - 1
+
+    # Insert to HEAD
+    my_list.insert("INSERT_2", 0)
+    # print(my_list, "Size: {}".format(len(my_list)))
+    assert [i for i in my_list] == [
+        "INSERT_2",
+        "ADD_5",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        "ADD_1",
+        1,
+        2,
+        3,
+        4,
+        5,
+        "INSERT_1",
+    ]
+    assert my_list.tail.item == "INSERT_1"
+    assert len(my_list) == 12
+    assert my_list.index("INSERT_1") == len(my_list) - 1
+    assert my_list.index("INSERT_2") == 0
+
+    # Insert to MIDDLE
+    my_list.insert("INSERT_3", 5)
+    # print(my_list, "Size: {}".format(len(my_list)))
+    assert [i for i in my_list] == [
+        "INSERT_2",
+        "ADD_5",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        "INSERT_3",
+        "ADD_1",
+        1,
+        2,
+        3,
+        4,
+        5,
+        "INSERT_1",
+    ]
+    assert my_list.tail.item == "INSERT_1"
+    assert my_list.head.item == "INSERT_2"
+    assert len(my_list) == 13
+    assert my_list.index("INSERT_1") == len(my_list) - 1
+    assert my_list.index("INSERT_2") == 0
+    assert my_list.index("INSERT_3") == 5
+    assert my_list.index("ADD_1") == 6
+
+    # POP
+
+    # POP from TAIL
+    res1 = my_list.pop()
+    # print(my_list, "Size: {}".format(len(my_list)))
+    assert res1 == "INSERT_1"
+    assert my_list.head.item == "INSERT_2"
+    assert my_list.tail.item == 5
+    assert len(my_list) == 12
+    assert [i for i in my_list] == [
+        "INSERT_2",
+        "ADD_5",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        "INSERT_3",
+        "ADD_1",
+        1,
+        2,
+        3,
+        4,
+        5,
+    ]
+    assert my_list.index(5) == len(my_list) - 1
+
+    # POP from HEAD
+    res1 = my_list.pop(0)
+    # print(my_list, "Size: {}".format(len(my_list)))
+    assert res1 == "INSERT_2"
+    assert my_list.head.item == "ADD_5"
+    assert my_list.tail.item == 5
+    assert len(my_list) == 11
+    assert [i for i in my_list] == [
+        "ADD_5",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        "INSERT_3",
+        "ADD_1",
+        1,
+        2,
+        3,
+        4,
+        5,
+    ]
+    assert my_list.index(5) == len(my_list) - 1
+
+    # POP from MIDDLE
+    idx = 5
+    res1 = my_list.pop(idx)
+    # print(my_list, "Size: {}".format(len(my_list)))
+    assert res1 == "ADD_1"
+    assert my_list.head.item == "ADD_5"
+    assert my_list.tail.item == 5
+    assert len(my_list) == 10
+    assert my_list.index(1) == idx
+    assert [i for i in my_list] == [
+        "ADD_5",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        "INSERT_3",
+        1,
+        2,
+        3,
+        4,
+        5,
+    ]
+    assert my_list.index(5) == len(my_list) - 1
+
+    # REMOVE
+    my_list.remove("INSERT_3")
+    # print(my_list, "Size: {}".format(len(my_list)))
+    assert my_list.head.item == "ADD_5"
+    assert my_list.tail.item == 5
+    assert len(my_list) == 9
+    assert [i for i in my_list] == ["ADD_5", "ADD_4", "ADD_3", "ADD_2", 1, 2, 3, 4, 5]
+
+    # ADD again!
+    my_list.add("NEW_ADD")
+    # print(my_list, "Size: {}".format(len(my_list)))
+    assert my_list.head.item == "NEW_ADD"
+    assert my_list.tail.item == 5
+    assert len(my_list) == 10
+    assert [i for i in my_list] == [
+        "NEW_ADD",
+        "ADD_5",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        1,
+        2,
+        3,
+        4,
+        5,
+    ]
+    assert my_list.index("NEW_ADD") == 0
+
+    # APPEND again!
+    my_list.append("NEW_APPEND")
+    # print(my_list, "Size: {}".format(len(my_list)))
+    assert my_list.head.item == "NEW_ADD"
+    assert my_list.tail.item == "NEW_APPEND"
+    assert len(my_list) == 11
+    assert [i for i in my_list] == [
+        "NEW_ADD",
+        "ADD_5",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        1,
+        2,
+        3,
+        4,
+        5,
+        "NEW_APPEND",
+    ]
+    assert my_list.index("NEW_APPEND") == len(my_list) - 1
+
+    # REMOVE again!
+    my_list.remove(5)
+    # print(my_list, "Size: {}".format(len(my_list)))
+    assert my_list.head.item == "NEW_ADD"
+    assert my_list.tail.item == "NEW_APPEND"
+    assert len(my_list) == 10
+    assert [i for i in my_list] == [
+        "NEW_ADD",
+        "ADD_5",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        1,
+        2,
+        3,
+        4,
+        "NEW_APPEND",
+    ]
+    assert my_list.index("NEW_APPEND") == len(my_list) - 1
+
+    # POP again!
+    res = my_list.pop(1)
+    # print(my_list, "Size: {}".format(len(my_list)))
+    assert res == "ADD_5"
+    assert my_list.head.item == "NEW_ADD"
+    assert my_list.tail.item == "NEW_APPEND"
+    assert len(my_list) == 9
+    assert my_list.index("ADD_4") == 1
+    assert [i for i in my_list] == [
+        "NEW_ADD",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        1,
+        2,
+        3,
+        4,
+        "NEW_APPEND",
+    ]
+    assert my_list.index("NEW_APPEND") == len(my_list) - 1
+
+    # INSERT by NEGATIVE index!
+    my_list.insert("NEGATIVE", -1)
+    # print(my_list, "Size: {}".format(len(my_list)))
+    assert my_list.head.item == "NEGATIVE"
+    assert my_list.tail.item == "NEW_APPEND"
+    assert len(my_list) == 10
+    assert [i for i in my_list] == [
+        "NEGATIVE",
+        "NEW_ADD",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        1,
+        2,
+        3,
+        4,
+        "NEW_APPEND",
+    ]
+    assert my_list.index("NEW_APPEND") == len(my_list) - 1
+
+    # Contains
+    for i in (
+        "NEGATIVE",
+        "NEW_ADD",
+        "ADD_4",
+        "ADD_3",
+        "ADD_2",
+        1,
+        2,
+        3,
+        4,
+        "NEW_APPEND",
+    ):
+        assert i in my_list
+
+    return my_list
+
+
+def check_exceptions(my_list):
+    print("\nCheck possible Exceptions:\n")
+
+    try:
+        LinkedList().pop()
+    except IndexError:
+        print("- IndexError on 'pop' from empty list is raised")
+
+    try:
+        my_list.pop(15)
+    except IndexError:
+        print("- IndexError on 'pop' by non-existing index is raised")
+
+    try:
+        my_list.remove("sdfsf3w590453045jjk")
+    except ValueError:
+        print("- ValueError upon 'remove' of non-existing field is raised")
+
+
+linked_list = LinkedList()
+linked_list = check_linked_list_methods(linked_list)
+check_exceptions(linked_list)
